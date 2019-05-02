f29b4ecf1e47d79512249a68d6b2875aef82be9d
RMF2PT <ricardomfferreira@gmail.com>
2019-05-02 23:23:37 +0100

    Create SearchBox Component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, username, email }) => {
 	return (
-		<div className="bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
-			<img src="https://robohash.org/name?size=200x200" alt="robots" />
+		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img src={`https://robohash.org/${username}?size=200x200`} alt="robots" />
 			<div>
-				<h2>Name</h2>
-				<p>Email</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
