5a35c60b006e25e17314a74966efb9c2a5e6ac2f
RMF2PT <ricardomfferreira@gmail.com>
2019-05-03 23:12:49 +0100

    Work on SearchBox and Style the App

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const state = {
-	robots: robots,
-	searchfield: ''
-};
-
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		};
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	};
+
 	render() {
+		const filteredRobot = this.state.robots.filter((robot) => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
 		return (
 			<div className="tc">
-				<h1>RobotFriends</h1>
-				<SearchBox />
-				<CardList robots={robots} />;
+				<h1 className="f1">RobotFriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robots={filteredRobot} />
 			</div>
 		);
 	}
