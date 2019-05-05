6c9b7468cc0931f526cdf0987ae38dfa3c2ea371
RMF2PT <ricardomfferreira@gmail.com>
2019-05-05 18:27:28 +0100

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import 'tachyons';
 import * as serviceWorker from './serviceWorker';
-
+import 'tachyons';
+import './styles/index.css';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
