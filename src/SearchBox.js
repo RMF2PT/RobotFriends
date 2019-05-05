510a3a58b8102f69a330823d611bd2909526443f
RMF2PT <ricardomfferreira@gmail.com>
2019-05-05 16:52:45 +0100

    Resolve ESLint errors

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 const SearchBox = ({ searchChange }) => {
 	return (
 		<div className="pa2">
-			<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" onChange={searchChange}/>
+			<input
+				className="pa3 ba b--green bg-lightest-blue"
+				type="search"
+				placeholder="search robots"
+				onChange={searchChange}
+			/>
 		</div>
 	);
 };
